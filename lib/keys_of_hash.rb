3bd6ec65b183d92f964984817f45012b668ecaff
Manuel Grullon <astermanuelg@gmail.com>
2018-03-05 00:12:19 -0500

    I feel like self really should of been mentioned at some point any where

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    result = []
+    arguments.each do |argument|
+      self.each do |key, value|
+        if value == argument
+          result << key
+        end
+      end
+    end
+    result
   end
 end
